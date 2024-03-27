c8464070643c2cd979be6a6ab76e6d2180178601
chloeqxq <55366902+chloeqxq@users.noreply.github.com>
2024-03-27 15:19:29 -0400

    Add Frontend UI

---
+++ src/main/frontend/pages/components/SearchBar.js
@@ -0,0 +1,15 @@
+export default function SearchBar() {
+    return (
+      <div className="container">
+        <div className="search-bar">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="search-input"
+          />
+          <button className="search-button">Search</button>
+        </div>
+      </div>
+    );
+  }
+  
